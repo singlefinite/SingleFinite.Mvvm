e37bbd107fe58565e79a7c17c1691b3af8ddee8c
Nate Wallace <natewallace@gmail.com>
2024-11-05 15:39:23 -0500

    Update company name in license

--- SingleFinite.Mvvm.UnitTests/ViewModelTests.cs
+++ SingleFinite.Mvvm.UnitTests/ViewModelTests.cs
@@ -1,5 +1,5 @@
 ﻿// MIT License
-// Copyright (c) 2024 SingleFinite
+// Copyright (c) 2024 Single Finite
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
 // files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
